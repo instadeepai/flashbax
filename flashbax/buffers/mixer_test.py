c6eee8f0791f2a03b7785e2032498eabc9d6e4f4
Callum Tilbury <c.tilbury@instadeep.com>
2024-07-17 15:00:00 +0200

    feat: tests for mixer.

--- flashbax/buffers/mixer_test.py
+++ flashbax/buffers/mixer_test.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from copy import deepcopy
-from functools import partial
 
 import chex
 import jax
@@ -27,7 +25,7 @@ from flashbax.buffers import (
     trajectory_buffer,
 )
 from flashbax.buffers.conftest import get_fake_batch
-from flashbax.buffers.mixed_buffer import joint_mixed_add, mixed_sample
+from flashbax.buffers.mixer import make_mixer
 
 
 @pytest.fixture
@@ -89,8 +87,8 @@ def test_mixed_trajectory_sample(
     sample_sequence_length,
     fake_transition,
 ):
+    buffers = []
     buffer_states = []
-    buffer_sample_fns = []
     for i in range(3):
         buffer = trajectory_buffer.make_trajectory_buffer(
             max_length_time_axis=200 * (i + 1),
@@ -100,19 +98,26 @@ def test_mixed_trajectory_sample(
             sample_sequence_length=sample_sequence_length,
             period=sample_period,
         )
+        buffers.append(buffer)
+
         state = buffer.init(
-            jax.tree_map(lambda x: jnp.ones_like(x) * i, fake_transition)
+            jax.tree_map(lambda x, _i=i: jnp.ones_like(x) * _i, fake_transition)
         )
         fake_add_data = get_fake_batch_sequence(fake_transition, add_batch_size, 50)
-        fake_add_data = jax.tree_map(lambda x: jnp.ones_like(x) * i, fake_add_data)
+        fake_add_data = jax.tree_map(
+            lambda x, _i=i: jnp.ones_like(x) * _i, fake_add_data
+        )
         state = buffer.add(state, fake_add_data)
         buffer_states.append(state)
-        buffer_sample_fns.append(buffer.sample)
 
     proportions = [0.2, 0.2, 0.6]
-    samples = mixed_sample(
-        buffer_states, rng_key, buffer_sample_fns, proportions, sample_batch_size
+    mixer = make_mixer(
+        buffers=buffers,
+        proportions=proportions,
+        sample_batch_size=sample_batch_size,
     )
+    samples = mixer.sample(buffer_states, rng_key)
+
     assert samples is not None
     expected_zeros = int(sample_batch_size * proportions[0])
     expected_ones = int(sample_batch_size * proportions[1])
@@ -133,8 +138,8 @@ def test_mixed_prioritised_trajectory_sample(
     sample_sequence_length,
     fake_transition,
 ):
+    buffers = []
     buffer_states = []
-    buffer_sample_fns = []
     for i in range(3):
         buffer = prioritised_trajectory_buffer.make_prioritised_trajectory_buffer(
             max_length_time_axis=200 * (i + 1),
@@ -144,19 +149,26 @@ def test_mixed_prioritised_trajectory_sample(
             sample_sequence_length=sample_sequence_length,
             period=sample_period,
         )
+        buffers.append(buffer)
+
         state = buffer.init(
-            jax.tree_map(lambda x: jnp.ones_like(x) * i, fake_transition)
+            jax.tree_map(lambda x, _i=i: jnp.ones_like(x) * _i, fake_transition)
         )
         fake_add_data = get_fake_batch_sequence(fake_transition, add_batch_size, 50)
-        fake_add_data = jax.tree_map(lambda x: jnp.ones_like(x) * i, fake_add_data)
+        fake_add_data = jax.tree_map(
+            lambda x, _i=i: jnp.ones_like(x) * _i, fake_add_data
+        )
         state = buffer.add(state, fake_add_data)
         buffer_states.append(state)
-        buffer_sample_fns.append(buffer.sample)
 
     proportions = [0.4, 0.1, 0.5]
-    samples = mixed_sample(
-        buffer_states, rng_key, buffer_sample_fns, proportions, sample_batch_size
+    mixer = make_mixer(
+        buffers=buffers,
+        proportions=proportions,
+        sample_batch_size=sample_batch_size,
     )
+    samples = mixer.sample(buffer_states, rng_key)
+
     assert samples is not None
     expected_zeros = int(sample_batch_size * proportions[0])
     expected_ones = int(sample_batch_size * proportions[1])
@@ -172,8 +184,8 @@ def test_mixed_prioritised_trajectory_sample(
 def test_mixed_flat_buffer_sample(
     rng_key, sample_batch_size, add_batch_size, fake_transition
 ):
+    buffers = []
     buffer_states = []
-    buffer_sample_fns = []
     for i in range(3):
         buffer = flat_buffer.make_flat_buffer(
             max_length=200 * (i + 1),
@@ -182,19 +194,26 @@ def test_mixed_flat_buffer_sample(
             add_batch_size=add_batch_size,
             add_sequences=True,
         )
+        buffers.append(buffer)
+
         state = buffer.init(
-            jax.tree_map(lambda x: jnp.ones_like(x) * i, fake_transition)
+            jax.tree_map(lambda x, _i=i: jnp.ones_like(x) * _i, fake_transition)
         )
         fake_add_data = get_fake_batch_sequence(fake_transition, add_batch_size, 50)
-        fake_add_data = jax.tree_map(lambda x: jnp.ones_like(x) * i, fake_add_data)
+        fake_add_data = jax.tree_map(
+            lambda x, _i=i: jnp.ones_like(x) * _i, fake_add_data
+        )
         state = buffer.add(state, fake_add_data)
         buffer_states.append(state)
-        buffer_sample_fns.append(buffer.sample)
 
     proportions = [0.1, 0.1, 0.8]
-    samples = mixed_sample(
-        buffer_states, rng_key, buffer_sample_fns, proportions, sample_batch_size
+    mixer = make_mixer(
+        buffers=buffers,
+        proportions=proportions,
+        sample_batch_size=sample_batch_size,
     )
+    samples = mixer.sample(buffer_states, rng_key)
+
     assert samples is not None
     expected_zeros = int(sample_batch_size * proportions[0])
     expected_ones = int(sample_batch_size * proportions[1])
@@ -206,35 +225,6 @@ def test_mixed_flat_buffer_sample(
     assert np.sum(dones == 2) == expected_twos
 
 
-def test_joint_mixed_add(rng_key, fake_transition, add_batch_size):
-    buffer_states = []
-    buffer_add_fns = []
-    for i in range(3):
-        buffer = trajectory_buffer.make_trajectory_buffer(
-            max_length_time_axis=2000 * (i + 1),
-            min_length_time_axis=0,
-            sample_batch_size=100,
-            add_batch_size=add_batch_size,
-            sample_sequence_length=2,
-            period=1,
-        )
-        state = buffer.init(
-            jax.tree_map(lambda x: jnp.ones_like(x) * i, fake_transition)
-        )
-        fake_add_data = get_fake_batch_sequence(fake_transition, add_batch_size, 50)
-        fake_add_data = jax.tree_map(lambda x: jnp.ones_like(x) * i, fake_add_data)
-        buffer_states.append(state)
-        buffer_add_fns.append(buffer.add)
-
-    fake_add_data = get_fake_batch_sequence(fake_transition, add_batch_size, 50)
-    fake_add_data = jax.tree_map(lambda x: jnp.ones_like(x) * 6, fake_add_data)
-    new_states = joint_mixed_add(buffer_states, fake_add_data, buffer_add_fns)
-    assert len(new_states) == len(buffer_states)
-    for state in new_states:
-        assert state is not None
-        assert jnp.sum(state.experience["done"] == 6) == add_batch_size * 50
-
-
 def test_mixed_buffer_does_not_smoke(
     rng_key,
     sample_batch_size,
@@ -243,9 +233,8 @@ def test_mixed_buffer_does_not_smoke(
     sample_sequence_length,
     fake_transition,
 ):
+    buffers = []
     buffer_states = []
-    buffer_sample_fns = []
-    buffer_add_fns = []
     for i in range(3):
         buffer = trajectory_buffer.make_trajectory_buffer(
             max_length_time_axis=2000 * (i + 1),
@@ -255,25 +244,26 @@ def test_mixed_buffer_does_not_smoke(
             sample_sequence_length=sample_sequence_length,
             period=sample_period,
         )
+        buffers.append(buffer)
+
         state = buffer.init(
-            jax.tree_map(lambda x: jnp.ones_like(x) * i, fake_transition)
+            jax.tree_map(lambda x, _i=i: jnp.ones_like(x) * _i, fake_transition)
         )
         fake_add_data = get_fake_batch_sequence(fake_transition, add_batch_size, 50)
-        fake_add_data = jax.tree_map(lambda x: jnp.ones_like(x) * i, fake_add_data)
+        fake_add_data = jax.tree_map(
+            lambda x, _i=i: jnp.ones_like(x) * _i, fake_add_data
+        )
         state = buffer.add(state, fake_add_data)
         buffer_states.append(state)
-        buffer_sample_fns.append(buffer.sample)
-        buffer_add_fns.append(buffer.add)
 
     proportions = [0.2, 0.2, 0.6]
-    # we expect to pre-instantiate the function with the buffer_sample_fns, proportions and sample_batch_size
-    mixed_sample_fn = partial(
-        mixed_sample,
-        buffer_sample_fns=buffer_sample_fns,
+    mixer = make_mixer(
+        buffers=buffers,
         proportions=proportions,
         sample_batch_size=sample_batch_size,
     )
-    samples = jax.jit(mixed_sample_fn)(buffer_states, rng_key)
+    samples = jax.jit(mixer.sample)(buffer_states, rng_key)
+
     assert samples is not None
     expected_zeros = int(sample_batch_size * proportions[0])
     expected_ones = int(sample_batch_size * proportions[1])
@@ -284,5 +274,3 @@ def test_mixed_buffer_does_not_smoke(
     assert np.sum(dones == 0) == expected_zeros
     assert np.sum(dones == 1) == expected_ones
     assert np.sum(dones == 2) == expected_twos
-
-    # mixed_joint_add_fn = partial(joint_mixed_add, buffer_add_fns=buffer_add_fns)
